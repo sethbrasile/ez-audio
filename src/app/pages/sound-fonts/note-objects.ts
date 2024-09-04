4936fb6a5291876d2328a9e04a741a18b5e29a92
Seth Brasile <seth.brasile@gmail.com>
2024-09-04 10:49:15 -0500

    cleanup

--- src/app/pages/sound-fonts/note-objects.ts
+++ src/app/pages/sound-fonts/note-objects.ts
@@ -15,17 +15,13 @@ note.name // 'Ab
 
 const Content = {
   setup() {
-    // setupPiano(document.querySelector<HTMLButtonElement>('#left')!, 'left')
-    // setupPiano(document.querySelector<HTMLButtonElement>('#right')!, 'right')
-    // setupPiano(document.querySelector<HTMLButtonElement>('#both')!, 'both')
   },
   html: `
-
 ${nav}
 
 <h1>Note Objects</h1>
 
-<p>Note objects return from the createFont method are very simple and just have a few properties.</p>
+<p>SampledNote objects returned from the createFont method are a very simple addition to Sound and just have a few additional properties.</p>
 <p>Using the note Ab1 as an example:</p>
 
 ${codeBlock(codeExample)}
