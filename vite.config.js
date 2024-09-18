bced59cfa7595cdc9be4ed3d9710b25601622e85
Seth Brasile <seth.brasile@gmail.com>
2024-09-18 13:56:34 -0500

    separate vite config for docs and lib

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,13 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import prismjs from 'vite-plugin-prismjs'
 import dtsPlugin from 'vite-plugin-dts'
 
 export default defineConfig({
-  base: '/ez-audio/',
   plugins: [
     tsconfigPaths(),
-    prismjs({
-      languages: ['typescript', 'html'],
-      plugins: ['line-numbers'],
-      theme: 'tomorrow',
-      css: true,
-    }),
     dtsPlugin(),
   ],
-  publicDir: './src/app/public',
   test: {
     environment: 'happy-dom',
   },
---
+++ vite.docs.config.js
@@ -0,0 +1,21 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import prismjs from 'vite-plugin-prismjs'
+
+export default defineConfig({
+  base: '/ez-audio/',
+  plugins: [
+    tsconfigPaths(),
+    prismjs({
+      languages: ['typescript', 'html'],
+      plugins: ['line-numbers'],
+      theme: 'tomorrow',
+      css: true,
+    }),
+  ],
+  publicDir: './src/app/public',
+  build: {
+    outDir: 'dist-docs',
+  },
+})
