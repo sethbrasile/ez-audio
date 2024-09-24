9761ba6e9411ec90c9097d4e190e580cfcfe440d
Seth Brasile <seth.brasile@gmail.com>
2024-09-24 16:48:38 -0500

    drum machine on index

--- src/app/components/permission-banner.ts
+++ src/app/components/permission-banner.ts
@@ -6,7 +6,7 @@ export default {
     initAudio().then(() => document.querySelector<HTMLDivElement>('#init-banner')!.classList.add('hidden'))
 
     // TODO: the button literally doesn't even need to do anything. If the user clicks ANY button, the audiocontext is allowed to start.
-    const initButton = document.querySelector<HTMLButtonElement>('#init')!
+    const initButton = document.querySelector<HTMLButtonElement>('#init-button')!
     function init(): void {
       initAudio()
       initButton.removeEventListener('click', init)
@@ -17,10 +17,10 @@ export default {
   html: `
 <div id="init-banner" class="alert warning">
   <h3>${inlineCode('AudioContext')} Not Started!</h3>
-  <p>This page uses the "obtain permission" method of starting the browser's ${inlineCode('AudioContext')}</p>
+  <p>This example uses the "obtain permission" method of starting the browser's ${inlineCode('AudioContext')}</p>
   <p>You can read more about starting the ${inlineCode('AudioContext')} <a href="/ez-web-audio/starting-audiocontext">HERE</a>
 
-  <div><button id="init">Initialize AudioContext</button></div>
+  <div><button id="init-button">Initialize AudioContext</button></div>
 </div>
 `,
 
--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -1,23 +1,16 @@
+import DrumMachine from '../components/drum-machine'
+import LoadingSpinner from '../components/loading-spinner'
+import PermissionBanner from '../components/permission-banner'
 import { codeBlock } from '../utils'
 import { createSound } from '@/index'
 
 const url = 'Eb5.mp3'
 
-const codeExample = `
-import { createSound } from 'ez-web-audio'
-
-async function playSound() {
-  // 1. Load a sound from a URL
-  const note = await createSound('Eb5.mp3')
-  // 2. Play the sound
-  note.play()
-}
-
-someButton.addEventListener('click', playSound)
-`
-
 const Content = {
-  setup() {
+  async setup() {
+    LoadingSpinner.show()
+    PermissionBanner.setup()
+
     const element = document.querySelector<HTMLButtonElement>('#play')!
 
     createSound(url)
@@ -28,6 +21,9 @@ const Content = {
     }
 
     element.addEventListener('click', play)
+
+    await DrumMachine.setup()
+    LoadingSpinner.hide()
   },
   html: `
 <h1>EZ Audio</h1>
@@ -39,10 +35,28 @@ which was developed before vue and react rose to prominence.</p>
 <p class="read-the-docs">This is a full rewrite  in vanilla typescript that exposes a similar-ish API to Ember Audio.</p>
 <p>EZ Audio provides classes and composable functions to make working with the web audio API super EZ.</p>
 <p>EZ Audio aims to simplify sampling, triggering, routing, scheduling, synthesizing, soundfonts, and working with audio in general.</p>
+<p>EZ Web Audio handles cases from the super simple...</p>
 
 <button id="play" type="button">Play</button>
+${codeBlock(`
+  import { createSound } from 'ez-web-audio'
 
-${codeBlock(codeExample)}
+async function playSound() {
+  // 1. Load a sound from a URL
+  const note = await createSound('Eb5.mp3')
+  // 2. Play the sound
+  note.play()
+}
+
+someButton.addEventListener('click', playSound)
+`)}
+
+<p>To honestly still pretty dang simple...</p>
+
+${LoadingSpinner}
+${PermissionBanner}
+${DrumMachine}
+
 `,
 }
 export default Content
